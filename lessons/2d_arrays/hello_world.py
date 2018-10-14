28a110ef667c268ef3a94a29c9cf43b104a184b3
kyle <kyle1james@gmail.com>
2018-10-14 11:08:46 -0400

    project

--- lessons/2d_arrays/hello_world.py
+++ lessons/2d_arrays/hello_world.py
@@ -2,15 +2,9 @@ import random
 my_nums = [1,2,3,4,5,6,7,8,9,10]
 
 # access the 5th element in my_nums
-# print(my_nums[4])
 
 # create a sequential search function for my nums
-def sequential_search(arr, target):
-    ans = False
-    for num in my_nums:
-        if num == target:
-            ans = True
-    return ans
+
 # Explain how a binary search would work on my_nums
 
 
@@ -20,30 +14,25 @@ sorted_matrix = [[col for col in range(6)] for row in range(10)]
 # random ints
 matrix = [[random.randint(0,100) for col in range(5)] for row in range(3)]
 # access the number 0 in the first row
-#print(sorted_matrix[0][0])
 
 # access the last number in the last row
-#print(sorted_matrix[-1][-1])
 
 # save the middle col index value in a var
-mid_col = len(sorted_matrix[0])//2
+
 
 # save the middle row index value in a var
-mid_row = len(sorted_matrix)//2
+
 
 # access the middle number in the middle row
-print(sorted_matrix[mid_row][mid_col])
+
 
 # Mild: create a function that will take in a 2D array and find the middle value
-def find_mid(matrix):
-    mid_col = len(matrix[0])//2
-    mid_row = len(matrix)//2
-    print(matrix[mid_row][mid_col])
-find_mid(matrix)
-print(matrix)
+
 
 # Spicy: print the entire middle row
+
 # Spicy: print the first value of every row
+
 # Spicy: take in two 2D arrays and find the middle value of both
 
 # Iterating over 2D arrays
@@ -57,7 +46,7 @@ my_nums = [[random.randint(0,500) for col in range(5)] for row in range(3)]
 
 # spicy plus: find the mean for my_nums
 
-
+# Complete at least one. Create user documentation for how your function works:
 
 # snail: https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1
 # flatten: https://www.codewars.com/kata/flatten-and-sort-an-array
--- lessons/linked_lists/README.md
+++ lessons/linked_lists/README.md
@@ -1,9 +1,9 @@
-# about linked lists
+# Learn about linked lists
 # http://ib.compscihub.net/wp-content/uploads/2018/07/5.1.12.pdf
 # https://www.youtube.com/watch?v=pBrz9HmjFOs
-# sketch single, double, circular
+# sketch single, double, and circular linked lists
 # http://ib.compscihub.net/wp-content/uploads/2018/07/5.1.13.pdf
-# array vs linked list
+# Know the difference between an array vs linked list
 # https://www.youtube.com/watch?v=lC-yYCOnN8Q&index=4&list=PL2_aWCzGMAwI3W_JlcBbtYTwiQSsOTa6P
 # question to test
 # http://ib.compscihub.net/wp-content/uploads/2018/07/5.1.12.pdf
